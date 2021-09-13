41dac5a9466c1262bd001135b0cca510e02df4ce
blackas <blackas119@gmail.com>
2021-09-14 02:42:22 +0900

    Updates

---
+++ precache-manifest.2533c66b622f03ff64e37a3238f56c5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aec398f526a7e0ce337",
+    "url": "/testreactweb/static/js/main.9e65aad9.chunk.js"
+  },
+  {
+    "revision": "5f4cb065c45fb0c3dcc6",
+    "url": "/testreactweb/static/js/runtime~main.4cf1499d.js"
+  },
+  {
+    "revision": "a138feb82360cb7f42a2",
+    "url": "/testreactweb/static/js/2.9f2d15d3.chunk.js"
+  },
+  {
+    "revision": "54f743b0d0ef20a64e87",
+    "url": "/testreactweb/static/js/3.b77c858b.chunk.js"
+  },
+  {
+    "revision": "34ba01b8726d6bd43485",
+    "url": "/testreactweb/static/js/4.e146fa5b.chunk.js"
+  },
+  {
+    "revision": "bfad532fc3a6d0b80db9",
+    "url": "/testreactweb/static/js/5.fc3c1fbb.chunk.js"
+  },
+  {
+    "revision": "09a3bf24c65f46b400f82d7351724522",
+    "url": "/testreactweb/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/testreactweb/precache-manifest.5507968911a63db63bdccc3fb64be3a8.js"
+  "/testreactweb/precache-manifest.2533c66b622f03ff64e37a3238f56c5f.js"
 );
 
 workbox.clientsClaim();

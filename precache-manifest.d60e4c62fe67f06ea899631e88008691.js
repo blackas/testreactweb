279fe8d2400d96ea45fab897cd6a0ea2b628b87a
blackas <blackas119@gmail.com>
2022-03-05 00:42:43 +0900

    Updates

---
+++ precache-manifest.d60e4c62fe67f06ea899631e88008691.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8708eb7aef8ea9de5351",
+    "url": "/testreactweb/static/js/main.e5757c14.chunk.js"
+  },
+  {
+    "revision": "8132fd3f63396d593219",
+    "url": "/testreactweb/static/js/runtime~main.7c6fd1b0.js"
+  },
+  {
+    "revision": "07b51be5ca2fc72b63ca",
+    "url": "/testreactweb/static/css/2.77315ed2.chunk.css"
+  },
+  {
+    "revision": "07b51be5ca2fc72b63ca",
+    "url": "/testreactweb/static/js/2.ffbdbfa3.chunk.js"
+  },
+  {
+    "revision": "a25c1ac396d53c5be167",
+    "url": "/testreactweb/static/js/3.86dcb266.chunk.js"
+  },
+  {
+    "revision": "facf45adc7862594424a",
+    "url": "/testreactweb/static/js/4.7b919557.chunk.js"
+  },
+  {
+    "revision": "17726f4d454bebbfd444",
+    "url": "/testreactweb/static/js/5.46d0a08b.chunk.js"
+  },
+  {
+    "revision": "f213683df2c2384a98f68a4947eae7f6",
+    "url": "/testreactweb/index.html"
+  }
+];
